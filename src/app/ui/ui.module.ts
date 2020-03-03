54fed6f4e44ebd79c1b0d6a3b99ed3f329d18d06
MEZZAKI <ezzakimouslih@gmail.com>
2020-03-03 11:34:53 +0100

    commit ng content class

--- src/app/ui/ui.module.ts
+++ src/app/ui/ui.module.ts
@@ -1,10 +1,18 @@
-import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { NgModule } from '@angular/core';
+import { UiComponent } from './components/ui/ui.component';
+import { NavComponent } from './components/nav/nav.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { HeaderComponent } from './components/header/header.component';
+
+
+
 
 
 
 @NgModule({
-  declarations: [],
+  declarations: [UiComponent, HeaderComponent, NavComponent, FooterComponent],
+  exports: [UiComponent,HeaderComponent, NavComponent, FooterComponent],
   imports: [
     CommonModule
   ]
